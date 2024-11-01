4eafa8740bad3aa20ff7f2f7596115c49a7c311e
ndakalajulius <juliusndakala98@gmail.com>
2024-11-01 15:41:34 +0300

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import BookDetailPage from './pages/BookDetailPage';
+import HomePage from '../pages/HomePage';
+import BookDetailPage from '../pages/BookDetailPage';
 
 function App() {
     return (
